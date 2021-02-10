15ebad721c71bec8f47166041406329c923aa433
houghtelin <josh@findsomehelp.com>
2021-02-10 18:22:01 -0500

    Start winning on the first try!  #WINNING

--- docker/startup.sh
+++ docker/startup.sh
@@ -2,4 +2,8 @@
 
 sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 
-/usr/bin/supervisord -c /app/docker/supervisord.conf
+php-fpm -D
+
+while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
+
+nginx
